cd2bcb69a6e95b56c2014cc7824225e9e37cfecd
Study_work <144671622+Tictaker3042@users.noreply.github.com>
2024-02-15 18:45:24 +0300

    Work

--- src/Complex.kt
+++ src/Complex.kt
@@ -1,28 +1,47 @@
-data class Complex(private var numerator: Double, private var denominator: Double) {
+import kotlin.math.pow
+
+data class Complex(private var real: Double, private var imaginary: Double) {
     init {
 
     }
 
-    fun plus(secondFraction: Complex) {
-        val a = numerator
-        val b = denominator
-        val c = secondFraction.numerator
-        val d = secondFraction.denominator
-        return Complex(a + c, b + d)
+    operator fun plus(other: Complex): Complex {
+        val newReal = this.real + other.real
+        val imaginary = this.imaginary + other.imaginary
+        return Complex(newReal, imaginary)
     }
 
-    fun minus(secondFraction: Complex): Complex {
-
+    operator fun minus(other: Complex): Complex {
+        val newReal = this.real - other.real
+        val newImaginary = this.imaginary - other.imaginary
+        return Complex(newReal, newImaginary)
     }
 
-    fun multiply(secondFraction: Complex): Complex {
-
+    operator fun times(other: Complex): Complex {
+        val newReal = this.real * other.real - this.imaginary * other.imaginary
+        val newImaginary = other.real * this.imaginary + this.real * other.imaginary
+        return Complex(newReal, newImaginary)
     }
 
-    fun divide(secondFraction: Complex): Complex {
+    operator fun div(other: Complex): Complex {
+        val newReal = -((this.real*other.real + this.imaginary*other.imaginary)/(other.real.pow(2) + other.imaginary.pow(2)))
+        val newImaginary = ((other.real*this.imaginary - this.real*other.imaginary)/(other.real.pow(2) + other.imaginary.pow(2)))
+        return Complex(newReal, newImaginary)
+    }
 
+    override fun toString(): String {     /*переопределяет строковое представление об объекте*/
+        return "$real + $imaginary"
     }
 }
 
-
+fun main() {
+    val first = Complex(1.0, 3.0)
+    val second = Complex(1.0, 4.0)
+    println("a = $first")
+    println("b = $second")
+    println("a + b = ${first + second}")
+    println("a - b = ${first - second}")
+    println("a * b = ${first * second}")
+    println("a / b = ${first / second}")
+}
 
